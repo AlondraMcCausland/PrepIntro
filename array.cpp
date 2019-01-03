10dac9b834f175ce61563b940d3b3ca1e8d93671
Alondra <alondrah0021@yahoo.com>
2019-01-02 19:29:21 -0800

    CAcommit

---
+++ array.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int alondra[7] = {66,75,4,43,99,32,45};
+
+  cout << alondra[3] << endl;
+
+  return 0;
+}
---
+++ cal.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+
+{
+  int num1, num2;
+  cout << ""
+
+  
+  return 0;
+}
